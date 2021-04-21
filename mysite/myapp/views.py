17e30968639a87ff1438afb15c1531ae63de6d4c
Chinmay Savanur <csavanur@umich.edu>
2021-04-20 21:25:52 -0400

    remove debug prints

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -63,8 +63,6 @@ def generate_token(request):
     password = request.headers["password"]
     if username is None or password is None:
         return Response({'error': 'Please provide both username and password'}, status=HTTP_400_BAD_REQUEST)
-    print(username)
-    print(password)
     user = authenticate(username = username, password = password)
     if not user:
         return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
